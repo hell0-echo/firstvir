617c9b9874c4815e5ccb1d68a087a038b28367d1
hell0-echo <djh13479873195@outlook.com>
2026-04-07 14:15:25 +0800

    Delete typegame directory

--- typegame/tg_apple_state_playing.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include "tg_apple_state_playing.h"
-#include "tg_apple_logic_orchestrator.h"
-TgAppleStatePlaying::TgAppleStatePlaying(TgAppleLogicOrchestrator& logic) : m_logic(logic) {}
-void TgAppleStatePlaying::onEnter() { m_spawnAcc = 0.0f; m_logic.spawnApple(); }
-void TgAppleStatePlaying::onUpdate(float dt) {
-    m_spawnAcc += dt;
-    if (m_spawnAcc >= 0.8f) { m_logic.spawnApple(); m_spawnAcc = 0.0f; }
-}
-void TgAppleStatePlaying::onExit() {}
