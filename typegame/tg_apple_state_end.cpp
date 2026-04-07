3eba644932e489129093070df68294b49c3cc3c3
hell0-echo <djh13479873195@outlook.com>
2026-04-07 15:19:20 +0800

    Delete typegame directory

--- typegame/tg_apple_state_end.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "tg_apple_state_end.h"
-#include "tg_apple_logic_orchestrator.h"
-TgAppleStateEnd::TgAppleStateEnd(TgAppleLogicOrchestrator& logic) : m_logic(logic) {}
-void TgAppleStateEnd::onEnter() {}
-void TgAppleStateEnd::onUpdate(float dt) { Q_UNUSED(dt); }
-void TgAppleStateEnd::onExit() {}
--- typegame/tg_apple_state_paused.cpp
+++
@@ -1,6 +0,0 @@
-﻿#include "tg_apple_state_paused.h"
-#include "tg_apple_logic_orchestrator.h"
-TgAppleStatePaused::TgAppleStatePaused(TgAppleLogicOrchestrator& logic) : m_logic(logic) {}
-void TgAppleStatePaused::onEnter() {}
-void TgAppleStatePaused::onUpdate(float dt) { Q_UNUSED(dt); }
-void TgAppleStatePaused::onExit() {}
